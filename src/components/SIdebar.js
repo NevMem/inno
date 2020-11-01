f10e5f0ab80be4ae0ae3fd615a7c57684c9d7374
yaigor <yaigor@yandex-team.ru>
2020-11-01 10:43:29 +0300

    Logo + text

--- src/components/SIdebar.js
+++ src/components/SIdebar.js
@@ -5,7 +5,20 @@ import { Link } from 'react-router-dom';
 export default class Sidebar extends Component {
     render() {
         return (
-            <div style={{position: "fixed", top: "120px", left: "30px"}}>
+            <div style={{position: "fixed", top: "90px", left: "30px"}}>
+                <div>
+                    <img
+                        style={{width: "70px", marginBottom: '20px', marginLeft: '10px'}}
+                        src="https://www.mos.ru/upload/structure/institutions/icon/gerald_msc2x(7).png" alt="logo" />
+                    <div style={{float: 'right', marginLeft: '10px'}}>
+                        <div style={{width: '70px'}}>
+                            Департамент культуры
+                        </div>
+                        <div>
+                            города Москвы
+                        </div>
+                    </div>
+                </div>
                 <div>
                     <Link className="sidebar-link" to="/search">Книги</Link>
                 </div>
